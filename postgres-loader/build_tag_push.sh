b913389346bf5965ace5cfa14d715865685f5deb
Peter Siemen <peter.siemen@googlemail.com>
2020-10-29 10:04:56 +0100

    cleanup

--- postgres-loader/build_tag_push.sh
+++ postgres-loader/build_tag_push.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-IMAGE_TAG=test
+IMAGE_TAG=0.1.0
 REPO=petersiemen
 NAME=snowplow-postgres-loader
 
 docker build -t ${NAME}:${IMAGE_TAG} .
 docker tag ${NAME}:${IMAGE_TAG} ${REPO}/${NAME}:${IMAGE_TAG}
-#docker push ${REPO}/${NAME}:${IMAGE_TAG}
+docker push ${REPO}/${NAME}:${IMAGE_TAG}
